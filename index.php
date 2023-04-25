d623ba39fabdbe9aae08f4125bfe7bceb054f965
Pedrogf23 <bleikerpro23@gmail.com>
2023-04-25 19:31:30 +0200

    25 4 23

--- index.php
+++ index.php
@@ -4,17 +4,16 @@
 session_start();
 
 // Archivos necesarios
-require_once 'Config/config.php';
-require_once 'Model/db.php';
-require_once 'Model/Cliente.php';
-require_once 'Model/LineaPedido.php';
-require_once 'Model/Lista.php';
-require_once 'Model/Pedido.php';
-require_once 'Model/Plataforma.php';
-require_once 'Model/Tarjeta.php';
-require_once 'Model/Videojuego.php';
-require_once 'Model/VideojuegoPlataforma.php';
-require_once 'Controller/Controller.php';
+require_once './Model/db.php';
+require_once './Model/Cliente.php';
+require_once './Model/LineaPedido.php';
+require_once './Model/Lista.php';
+require_once './Model/Pedido.php';
+require_once './Model/Plataforma.php';
+require_once './Model/Tarjeta.php';
+require_once './Model/Videojuego.php';
+require_once './Model/VideojuegoPlataforma.php';
+require_once './Controller/Controller.php';
 
 // Si no hay definida una acción, se pone la por defecto, definida en config/config.php
 if (!isset($_GET["action"])) $_GET["action"] = constant("DEFAULT_ACTION");
@@ -27,9 +26,9 @@ $datos = array();
 $datos = $controlador->{$_GET["action"]}(); // Llamada a las funciones del controlador, a través de la variable get action.
 
 // Includes de la vista
-include_once 'view/template/header.php';
-include_once 'view/' . $controlador->vista . '.php'; // Incluye la vita concreta, necesaria en cada momento, dependiendo del atrubuto view del objeto noteController.
-include_once 'view/template/footer.php';
+require_once 'view/template/header.php';
+require_once 'view/' . $controlador->vista . '.php'; // Incluye la vita concreta, necesaria en cada momento, dependiendo del atrubuto view del objeto noteController.
+require_once 'view/template/footer.php';
 
 
 ?>
