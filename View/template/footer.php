fb1b9f4f28e98a1a79ba31c93476edce6be81835
Pedrogf23 <bleikerpro23@gmail.com>
2023-04-25 12:48:17 +0200

    25 4 23

--- View/template/footer.php
+++ View/template/footer.php
@@ -1,10 +1,13 @@
     <script>
       // jQuery para cambiar entre clases
-      // Cada vez que se hace click en el elemento con la
-      // clase .contenedor_icono_burguer, se va intercambiando
-      // la clase .icono_cruz por .icono_barras y viceversa.
       $('.contenedor_icono_burger').on('click', function () {
+        // Cada vez que se hace click en el elemento con la
+        // clase .contenedor_icono_burguer, se va intercambiando
+        // la clase .icono_cruz por .icono_barras y viceversa.
         $(this).toggleClass('icono_cruz icono_barras');
+        // Además, también intercambia las clases on y off del
+        // elemento con clase .desplegable.
+        $('.desplegable').toggleClass('on off');
       });
     </script>
   </body>
--- View/template/header.php
+++ View/template/header.php
@@ -11,6 +11,10 @@
     <link rel="stylesheet" href="view/css/header.css" />
     <link rel="icon" href="img/logo.png" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@300;400;700&family=Press+Start+2P&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/3915de76d4.js" crossorigin="anonymous"></script>
   </head>
   
   <body>
@@ -27,19 +31,28 @@
       <div>
         <nav>
           <ul>
-            <li>PC</li>
-            <li>Xbox</li>
-            <li>PlayStation</li>
-            <li>Nintendo</li>
+            <li class="pc"><i class="fa-solid fa-desktop"></i>&nbsp;PC</li>
+            <li class="ps"><i class="fa-brands fa-playstation"></i>&nbsp;PlayStation</li>
+            <li class="xbox"><i class="fa-brands fa-xbox"></i>&nbsp;Xbox</li>
+            <li class="nintendo"><img class="nintendoIcon" src="img/icons/nintendo.svg">&nbsp;Nintendo</li>
           </ul>
         </nav>
       </div>
       <div>
-        <div class="contenedor_icono_burger icono_barras">
-          <span class=""></span>
-          <span class=""></span>
-          <span class=""></span>
-        </div>
-        <img src="img/logo.png" alt="Logotipo de la página.">
+        <i class="fa-solid fa-cart-shopping"></i>
+        <div class="profilePicture"></div>
       </div>
     </header>
+    <div class="desplegable off">
+        <div class="listas">
+          <i class="fa-solid fa-bookmark"></i>
+          <p>Biblioteca</p>
+          <i class="fa-solid fa-heart"></i>
+          <p>Favoritos</p>
+          <i class="fa-solid fa-cloud"></i>
+          <p>Deseados</p>
+          <i class="fa-solid fa-circle-plus"></i>
+          <p>Crear lista</p>
+        </div>
+        <div class="config"><i class="fa-solid fa-gear"></i>&nbsp;Configuración</div>
+    </div>
